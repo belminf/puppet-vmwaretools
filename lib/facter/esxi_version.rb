355b528736240d8c202806721397f67d21a03171
Belmin Fernandez <me@iambelmin.com>
2014-10-24 15:54:32 -0400

    formatting changes and using hash now (h/t jacob)

--- lib/facter/esxi_version.rb
+++ lib/facter/esxi_version.rb
@@ -1,34 +1,30 @@
 require 'facter'
 
 Facter.add(:esxi_version) do
-    confine :virtual => :vmware
-    confine :kernel => :Linux
-    setcode do
-        dmi_path = Facter::Util::Resolution.exec('which dmidecode 2> /dev/null')
-        if dmi_path.nil?
-            'unknown:no_dmidecode'
+  confine :virtual => :vmware
+  confine :kernel => :Linux
+  setcode do
+    bios_lookup_table = {
+      'E72C0' => '5.0',
+      'EA0C0' => '5.1',
+      'EA050' => '5.2'
+    }
+    bios_lookup_table.default = 'unknown:untranslated_version'
+    dmi_path = Facter::Util::Resolution.exec('which dmidecode 2> /dev/null')
+    if dmi_path.nil?
+      'unknown:no_dmidecode'
+    else
+      bios_output = Facter::Util::Resolution.exec("#{dmi_path} -t bios 2> /dev/null")
+      if bios_output.nil?
+        'unknown:dmidecode_empty_output'
+      else
+        bios_address_line = bios_output.match(/Address: 0x(?<addr>.*)/i)
+        if bios_address_line.nil?
+          'unknown:dmidecode_parsing_error'
         else
-            begin
-                bios_output = Facter::Util::Resolution.exec("#{dmi_path} -t bios 2> /dev/null")
-                if bios_output.nil?
-                    'unknown:dmidecode_empty_output'
-                else
-                    bios_address = bios_output.match(/Address: 0x(.*)/i)[1]
-                    case bios_address
-                        
-                        when 'E72C0'
-                            '5.0'
-                        when 'EA0C0'
-                            '5.1'
-                        when 'EA050'
-                            '5.5'
-                        else
-                            "unknown:unrecognized_bios_address:#{bios_address}"
-                    end
-                end
-            rescue
-                'unknown:dmidecode_parsing_error'
-            end
+          bios_lookup_table[bios_address_line[:addr]]
         end
+      end
     end
+  end
 end
