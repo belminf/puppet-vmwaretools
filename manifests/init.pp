2594eb812bfd29fc710d19426ff99ed0030a8b75
Belmin Fernandez <belmin.fernandez@cuny.edu>
2015-02-03 15:34:48 -0500

    re-factored

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,44 +5,40 @@
 
 class vmware::tools {
 
-	case "${osfamily} ${operatingsystemmajrelease}" {
-		/^RedHat (6|7)$/: {
-			$repo_url = "http://packages.vmware.com/tools/esx/${esxi_version}latest/rhel6/${architecture}/"
-			$service_cmd = '/etc/vmware-tools/init/vmware-tools-services'
-		}
-	}
+    $os_version = "${::osfamily} ${::operatingsystemmajrelease}"
+    if $os_version =~ /^RedHat (6|7)$/ {
+        $repo_url = "http://packages.vmware.com/tools/esx/${::esxi_version}latest/rhel6/${::architecture}/"
+        $service_cmd = '/etc/vmware-tools/init/vmware-tools-services'
+    } else {
+        fail("No repository for OS: ${os_version}")
+    }
 
-	if $repo_url {
-		yumrepo { 'vmware-osp':
-			baseurl => $repo_url,
-			descr => 'VMware OSD repository',
-			enabled => 1,
-			gpgcheck => 0,
-		}
+    yumrepo { 'vmware-osp':
+        baseurl  => $repo_url,
+        descr    => 'VMware OSD repository',
+        enabled  => 1,
+        gpgcheck => 0,
+    }
 
-		# Remove other tools
-		package { 'open-vm-tools':
-			ensure => absent,
-		}
+    # Remove other tools
+    package { 'open-vm-tools':
+        ensure => absent,
+    }
 
-		package { 'vmware-tools-esx-nox':
-			ensure => latest,
-			require => [
-				Yumrepo['vmware-osp'],
-				Package['open-vm-tools'],
-			],
-		}
+    package { 'vmware-tools-esx-nox':
+        ensure  => latest,
+        require => [
+            Yumrepo['vmware-osp'],
+            Package['open-vm-tools'],
+        ],
+    }
 
-		service { 'vmware-tools':
-			provider => 'base',
-			ensure => running,
-			start => "/usr/bin/sh -c '${service_cmd} start'",
-			stop => "/usr/bin/sh -c '${service_cmd} stop'",
-			status => "/usr/bin/sh -c '${service_cmd} status'",
-			restart => "/usr/bin/sh -c '${service_cmd} restart'",
-		}
-
-	} else {
-		notify { "OS not configured: ${osfamily} ${operatingsystemmajrelease}": }
-	}
+    service { 'vmware-tools':
+        ensure   => running,
+        provider => 'base',
+        start    => "/usr/bin/sh -c '${service_cmd} start'",
+        stop     => "/usr/bin/sh -c '${service_cmd} stop'",
+        status   => "/usr/bin/sh -c '${service_cmd} status'",
+        restart  => "/usr/bin/sh -c '${service_cmd} restart'",
+    }
 }
