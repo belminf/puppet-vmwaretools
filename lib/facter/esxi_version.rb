a4353869a49cde46ca53fd9f7fea9776c99527d7
Belmin Fernandez <me@iambelmin.com>
2015-02-04 14:34:48 -0500

    now compat with Ruby <1.9

--- lib/facter/esxi_version.rb
+++ lib/facter/esxi_version.rb
@@ -18,11 +18,11 @@ Facter.add(:esxi_version) do
       if bios_output.nil?
         'unknown:dmidecode_empty_output'
       else
-        bios_address_line = bios_output.match(/Address: 0x(?<addr>.*)/i)
+        bios_address_line = bios_output.match(/Address: 0x(.*)/i)
         if bios_address_line.nil?
           'unknown:dmidecode_parsing_error'
         else
-          bios_lookup_table[bios_address_line[:addr]]
+          bios_lookup_table[bios_address_line[1]]
         end
       end
     end
