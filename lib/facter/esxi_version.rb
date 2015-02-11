342fe5a6c45a12ff372defc0afed7394db0263c0
Belmin Fernandez <me@iambelmin.com>
2015-02-11 15:45:41 -0500

    added 4.0 and 4.1 + hard fails

--- lib/facter/esxi_version.rb
+++ lib/facter/esxi_version.rb
@@ -5,24 +5,25 @@ Facter.add(:esxi_version) do
   confine :kernel => :Linux
   setcode do
     bios_lookup_table = {
+      'EA550' => '4.0',
+      'EA2E0' => '4.1',
       'E72C0' => '5.0',
       'EA0C0' => '5.1',
       'EA050' => '5.5'
     }
-    bios_lookup_table.default = 'unknown:untranslated_version'
     dmi_path = Facter::Util::Resolution.exec('which dmidecode 2> /dev/null')
     if dmi_path.nil?
-      'unknown:no_dmidecode'
+      raise Puppet::ParserError, 'Unable to find dmidecode'
     else
       bios_output = Facter::Util::Resolution.exec("#{dmi_path} -t bios 2> /dev/null")
       if bios_output.nil?
-        'unknown:dmidecode_empty_output'
+        raise Puppet::ParseError, 'DMI did not return output'
       else
         bios_address_line = bios_output.match(/Address: 0x(.*)/i)
         if bios_address_line.nil?
-          'unknown:dmidecode_parsing_error'
+           raise Puppet::ParseError, 'DMI did not return address line'
         else
-          bios_lookup_table[bios_address_line[1]]
+          bios_lookup_table[bios_address_line[1]] or raise Puppet::ParseError, 'Address not mapped to an esxi version'
         end
       end
     end
