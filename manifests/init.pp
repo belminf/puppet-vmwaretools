97465869a603c4353e8843bc0f12176de87e05bf
Belmin Fernandez <belmin.fernandez@cuny.edu>
2015-03-06 14:51:11 -0500

    refactored to add RHEL7 support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,64 +1,84 @@
-# = Class: vmware::tools
+# = Class: vmwaretools
 #
-# Adds the proper OSP repository to a VMware VM. This is then used to install
-# VMware tools.
-#
+# Adds the proper VMware tools from either OSP or OS repository.
+
 # == Requires:
 #
 # Puppet 2.7 or greater (due to: class_inherits_from_params_class)
 #
 # == Sample Usage:
 #
-# class { 'vmware::tools': }
+# class { 'vmwaretools': }
 #
 
-class vmwaretools_osp (
-    $repo_url              = $vmwaretools_osp::params::repo_url,
-    $osp_packages          = $vmwaretools_osp::params::osp_packages,
-    $conflicting_packages  = $vmwaretools_osp::params::conflicting_packages,
-    $service_name          = $vmwaretools_osp::params::service_name,
-    $service_enable        = $vmwaretools_osp::params::service_enable,
-    $service_provider      = $vmwaretools_osp::params::service_provider,
-    $service_start         = $vmwaretools_osp::params::service_start,
-    $service_stop          = $vmwaretools_osp::params::service_stop,
-    $service_status        = $vmwaretools_osp::params::service_status,
-    $tarball_installer     = $vmwaretools_osp::params::tarball_installer,
-    $tarball_uninstall_opt = $vmwaretools_osp::params::tarball_uninstall_opt
-) inherits vmwaretools_osp::params {
+class vmwaretools (
+    $repo_url              = $vmwaretools::params::repo_url,
+    $required_packages     = $vmwaretools::params::required_packages,
+    $conflicting_packages  = $vmwaretools::params::conflicting_packages,
+    $service_name          = $vmwaretools::params::service_name,
+    $service_enable        = $vmwaretools::params::service_enable,
+    $service_provider      = $vmwaretools::params::service_provider,
+    $service_start         = $vmwaretools::params::service_start,
+    $service_stop          = $vmwaretools::params::service_stop,
+    $service_status        = $vmwaretools::params::service_status,
+    $tarball_installer     = $vmwaretools::params::tarball_installer,
+    $tarball_uninstall_opt = $vmwaretools::params::tarball_uninstall_opt
+) inherits vmwaretools::params {
 
-    yumrepo { 'vmware-osp':
-        baseurl  => $repo_url,
-        descr    => 'VMware OSP repository',
-        enabled  => 1,
-        gpgcheck => 0,
-    }
+    if $::virtual == 'vmware' {
 
-    if $tarball_installer {
-        exec { 'remove tarball':
-            command => "${tarball_installer}${tarball_uninstall_opt}",
-            onlyif  => "test -f ${tarball_installer}",
-            before  => Package[$osp_packages],
-            path    => '/usr/bin:/bin',
+        Package {
+            allow_virtual => false,
         }
-    }
 
-    package { $conflicting_packages:
-        ensure => absent,
-        before => Package[$osp_packages],
-    }
+        # Tarball cleanup
+        if $tarball_installer {
+            exec { 'remove tarball':
+                command => "${tarball_installer}${tarball_uninstall_opt}",
+                onlyif  => "test -f ${tarball_installer}",
+                before  => Package[$required_packages],
+                path    => '/usr/bin:/bin',
+            }
+        }
 
-    package { $osp_packages:
-        ensure  => latest,
-        require => Yumrepo['vmware-osp'],
-    }
+        # Uninstall conflicting
+        if $conflicting_packages {
+            package { $conflicting_packages:
+                ensure   => purged,
+                before   => Package[$required_packages],
+                provider => 'yum',
+            }
+        }
 
-    service { $service_name:
-        ensure   => running,
-        enable   => $service_enable,
-        provider => $service_provider,
-        start    => $service_start,
-        stop     => $service_stop,
-        status   => $service_status,
-        require  => Package[$osp_packages],
+        # Add repository
+        if $repo_url {
+            yumrepo { 'vmware-tools':
+                baseurl  => $repo_url,
+                descr    => 'VMware Tools',
+                enabled  => 1,
+                gpgcheck => 0,
+                before   => Package[$required_packages],
+            }
+        }
+    
+        # Add packages
+        if $required_packages {
+            package { $required_packages:
+                ensure  => latest,
+            }
+        }
+
+        # Manage service
+        if $service_name {
+            service { $service_name:
+                ensure   => running,
+                enable   => $service_enable,
+                provider => $service_provider,
+                start    => $service_start,
+                stop     => $service_stop,
+                status   => $service_status,
+                require  => Package[$required_packages],
+            }
+        }
     }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,46 +1,72 @@
-class vmwaretools_osp::params {
+class vmwaretools::params {
 
-    $os_version = "${::osfamily} ${::lsbmajdistrelease}"
+    $os_version = "${::osfamily} ${::operatingsystemmajrelease}"
     
-    # General settings
+    # Tarball cleanup
     $tarball_installer = '/etc/vmware-tools/installer.sh'
     $tarball_uninstall_opt = ' uninstall'
-    $conflicting_packages = [
-        'open-vm-tools',
-        'VMwareTools'
-    ]
-    $service_enable = true
-    
-    case $::architecture {
-        'x86_64': { $arch = 'x86_64' }
-        'i386':   { $arch = 'i686' }
-        default:  { fail("No support for ${::architecture}" ) }
-    }
 
-    # ESXi specific settings
-    case "${::esxi_version} - ${os_version}" {
-        /^5.(1|5) - RedHat (5|6)$/: {
-            $repo_url = "http://packages.vmware.com/tools/esx/${::esxi_version}latest/rhel${::lsbmajdistrelease}/${arch}/"
-            $osp_packages = [
+
+    # Default for service
+    $service_enable = true
+
+    # OS dependent
+    case $os_version {
+        /^RedHat 7$/: {
+            $conflicting_packages = [
+                'vmware-tools-core',
                 'vmware-tools-esx-nox',
-                'vmware-tools-esx-kmods',
-            ]
-            $service_name = 'vmware-tools-services'
-            if $os_version == 'RedHat 6' {
-                $service_provider = 'upstart'
-            }
-        }
-        /^4.(0|1) - RedHat (5|6)$/: {
-            $repo_url = "http://packages.vmware.com/tools/esx/${::esxi_version}latest/rhel${::lsbmajdistrelease}/${arch}/"
-            $osp_packages = [
                 'vmware-tools-nox',
-                'vmware-open-vm-tools-kmod',
             ]
-            $service_name = 'vmware-tools'
+            $required_packages = 'open-vm-tools'
         }
+
+        /^RedHat (5|6)$/: {
+
+            $conflicting_packages = [
+                'open-vm-tools',
+                'VMwareTools'
+            ]
+
+            case $::architecture {
+                'x86_64': { $arch = 'x86_64' }
+                'i386':   { $arch = 'i686' }
+                default:  { fail("No support for ${::architecture}" ) }
+            }
+        
+            # ESXi specific settings
+            case $::esxi_version {
+                /^5.(1|5)$/: {
+                    $repo_url = "http://packages.vmware.com/tools/esx/${::esxi_version}latest/rhel${::lsbmajdistrelease}/${arch}/"
+                    $required_packages = [
+                        'vmware-tools-esx-nox',
+                        'vmware-tools-esx-kmods',
+                    ]
+                    $service_name = 'vmware-tools-services'
+                    if $os_version == 'RedHat 6' {
+                        $service_provider = 'upstart'
+                    }
+                }
+                /^4.(0|1)$/: {
+                    $repo_url = "http://packages.vmware.com/tools/esx/${::esxi_version}latest/rhel${::lsbmajdistrelease}/${arch}/"
+                    $required_packages = [
+                        'vmware-tools-nox',
+                        'vmware-open-vm-tools-kmod',
+                    ]
+                    $service_name = 'vmware-tools'
+                }
+                default: {
+                    fail("No setup for ESXi and OS: ${::esxi_version} ${os_version}")
+                }
+            }
+            
+        }
+
         default: {
-            fail("No setup for ESXi and OS: ${::esxi_version} ${os_version}")
+            notice("Unsupported OS: ${os_version}")
         }
+        
     }
+    
 
 }
